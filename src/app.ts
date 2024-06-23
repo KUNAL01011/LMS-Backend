c972e315e1f605c358436ff87a7f8582691ce6f1
Kunal Kumar <kunal34255@gmail.com>
2024-06-23 22:29:45 +0530

    ErrorHandling functions

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { NextFunction, Request, Response } from 'express';
 export const app = express();
 import cors from "cors";
 import cookieParser from 'cookie-parser';
+import { ErrorMiddleware } from './middleware/error';
 
 
 //body parser
@@ -30,3 +31,5 @@ app.all("*", (req: Request, res: Response, next: NextFunction) => {
     err.statusCode = 404;
     next(err);
 });
+
+app.use(ErrorMiddleware);

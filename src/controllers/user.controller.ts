d7e9c0386dde5178f2dc82141653a1577e9e6005
Kunal Kumar <kunal34255@gmail.com>
2024-06-26 21:20:45 +0530

    login completed

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -6,6 +6,7 @@ import { CatchAsyncError } from "../middleware/catchAsyncError";
 import ejs from 'ejs';
 import path from "path";
 import sendMail from "../utils/sendMail";
+import { sendToken } from "../utils/jwt";
 
 //register user
 interface IRegistrationBody {
@@ -113,4 +114,41 @@ export const activateUser = CatchAsyncError(async (req: Request, res: Response, 
     catch (error: any) {
         return next(new ErrorHandler(error.message, 400));
     }
-})
+});
+
+// login user
+interface ILoginRequest {
+    email:string;
+    password:string;
+}
+
+
+export const loginUser = CatchAsyncError(async(req:Request,res:Response,next:NextFunction) => {
+    try {
+        const {email,password} = req.body as ILoginRequest;
+
+        if(!email || !password){
+            return next(new ErrorHandler("Please enter email and password",400));
+        }
+
+        const user = await userModel.findOne({email}).select("+password");
+
+        if(!user){
+            return next(new ErrorHandler("Invaild email or password",400));
+        };
+
+        const isPasswordMatch = await user.comparePassword(password);
+
+        if(!isPasswordMatch) {
+            return next(new ErrorHandler("Invaild email or password",400));
+        };
+
+        sendToken(user,200,res);
+
+    } catch (error:any) {
+        return next(new ErrorHandler(error.message,400));
+    }
+});
+
+// logout user
+
--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,76 +1,89 @@
-import mongoose,{Document,Model,Schema} from "mongoose";
+import mongoose, { Document, Model, Schema } from "mongoose";
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
-const emailRegexPattern:RegExp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+const emailRegexPattern: RegExp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
 export interface IUser extends Document {
-    name:string;
-    email:string;
-    password:string;
-    avatar:{
-        public_id:string;
-        url:string;
+    name: string;
+    email: string;
+    password: string;
+    avatar: {
+        public_id: string;
+        url: string;
     };
-    role:string;
-    isVerified:boolean;
-    courses:Array<{courseId:string}>;
-    comparePassword: (password:string) => Promise<boolean>;
+    role: string;
+    isVerified: boolean;
+    courses: Array<{ courseId: string }>;
+    comparePassword: (password: string) => Promise<boolean>;
+    SignAccessToken: () => string;
+    SignRefreshToken: () => string;
 };
 
 const userSchema: Schema<IUser> = new mongoose.Schema({
-    name:{
-        type:String,
-        required:[true,"Please enter your name"],
+    name: {
+        type: String,
+        required: [true, "Please enter your name"],
     },
     email: {
-        type:String,
-        required:[true,"Please enter your email"],
+        type: String,
+        required: [true, "Please enter your email"],
         validate: {
-            validator: function(value:string){
+            validator: function (value: string) {
                 return emailRegexPattern.test(value);
             },
-            message:"please enter a valid email",
+            message: "please enter a valid email",
         },
-        unique:true,
+        unique: true,
     },
     password: {
-        type:String,
-        required:[true,"Please enter your password"],
-        minlength:[6,"Please must be at least 6 characters"],
-        select:false
+        type: String,
+        required: [true, "Please enter your password"],
+        minlength: [6, "Please must be at least 6 characters"],
+        select: false
     },
-    avatar:{
-        public_id:String,
-        url:String,
+    avatar: {
+        public_id: String,
+        url: String,
     },
-    role:{
-        type:String,
-        default:"user",
+    role: {
+        type: String,
+        default: "user",
     },
-    isVerified:{
-       type:Boolean,
-       default:false 
+    isVerified: {
+        type: Boolean,
+        default: false
     },
-    courses:[
+    courses: [
         {
-            courseId:String,
+            courseId: String,
         }
     ],
-},{timestamps:true});
+}, { timestamps: true });
 
 // Hash Password before saving
-userSchema.pre<IUser>('save',async function(next){
-    if(!this.isModified('password')){
+userSchema.pre<IUser>('save', async function (next) {
+    if (!this.isModified('password')) {
         next();
     }
-    this.password = await bcrypt.hash(this.password,10);
+    this.password = await bcrypt.hash(this.password, 10);
     next();
 });
 
-//compare password
-userSchema.methods.comparePassword = async function(enterPassword:string):Promise<boolean>{
-    return await bcrypt.compare(enterPassword,this.password);
+//sign access token
+userSchema.methods.SignAccessToken = function () {
+    return jwt.sign({ id: this._id }, process.env.ACCESS_TOKEN || "");
 }
 
-const userModel:Model<IUser> = mongoose.model("User",userSchema);
+// sign refresh token 
+userSchema.methods.SignRefreshToken = function () {
+    return jwt.sign({ id: this._id }, process.env.REFRESH_TOKEN || '');
+}
+
+//compare password
+userSchema.methods.comparePassword = async function (enterPassword: string): Promise<boolean> {
+    return await bcrypt.compare(enterPassword, this.password);
+}
+
+const userModel: Model<IUser> = mongoose.model("User", userSchema);
 export default userModel;
